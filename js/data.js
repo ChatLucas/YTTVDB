1fcd77063af2aba98b9c0e8d498d38375ec2a1ca
ChatLucas <108232102+ChatLucas@users.noreply.github.com>
2024-04-29 09:13:26 +0200

    Catalogue: Add Code Lyoko
    Episode 1-26 FR

--- js/data.js
+++ js/data.js
@@ -21,6 +21,156 @@ var Catalogue = [
                         name: "Episode 1",
                         localName: {fr: "Episode 1 : Teddy Gozzila"},
                         ytids: {fr: "QCGixQKvTpA"}
+                    },
+                    {
+                        id: "e02",
+                        name: "Episode 2",
+                        localName: {fr: "Episode 2 : Le voir pour le croire"},
+                        ytids: {fr: "d3EiLxiGYgs"}
+                    },
+                    {
+                        id: "e03",
+                        name: "Episode 3",
+                        localName: {fr: "Episode 3 : Vacances dans la brume"},
+                        ytids: {fr: "I4AE3BvIxMQ"}
+                    },
+                    {
+                        id: "e04",
+                        name: "Episode 4",
+                        localName: {fr: "Episode 4 : Carnet de bord"},
+                        ytids: {fr: "5nsklx84-jE"}
+                    },
+                    {
+                        id: "e05",
+                        name: "Episode 5",
+                        localName: {fr: "Episode 5 : Big Bogue"},
+                        ytids: {fr: "SWnxzc-hOL8"}
+                    },
+                    {
+                        id: "e06",
+                        name: "Episode 6",
+                        localName: {fr: "Episode 6 : Cruel dilemme"},
+                        ytids: {fr: "rSmEPogTRI4"}
+                    },
+                    {
+                        id: "e07",
+                        name: "Episode 7",
+                        localName: {fr: "Episode 7 : Problème d'image"},
+                        ytids: {fr: "y4vHQJI9new"}
+                    },
+                    {
+                        id: "e08",
+                        name: "Episode 8",
+                        localName: {fr: "Episode 8 : Clap de fin"},
+                        ytids: {fr: "28l301P1Mt0"}
+                    },
+                    {
+                        id: "e09",
+                        name: "Episode 9",
+                        localName: {fr: "Episode 9 : Satellite"},
+                        ytids: {fr: "9ZXPk8nO_Io"}
+                    },
+                    {
+                        id: "e10",
+                        name: "Episode 10",
+                        localName: {fr: "Episode 10 : Créature de rêve"},
+                        ytids: {fr: "BGjXQYhMrfU"}
+                    },
+                    {
+                        id: "e11",
+                        name: "Episode 11",
+                        localName: {fr: "Episode 11 : Enragés"},
+                        ytids: {fr: "KiREBlUVBqM"}
+                    },
+                    {
+                        id: "e12",
+                        name: "Episode 12",
+                        localName: {fr: "Episode 12 : Attaque en pique"},
+                        ytids: {fr: "JMAWtZgReYg"}
+                    },
+                    {
+                        id: "e13",
+                        name: "Episode 13",
+                        localName: {fr: "Episode 13 : D'un cheveu"},
+                        ytids: {fr: "6QwFKmSx7M4"}
+                    },
+                    {
+                        id: "e14",
+                        name: "Episode 14",
+                        localName: {fr: "Episode 14 : Piège"},
+                        ytids: {fr: "xJOadVudax4"}
+                    },
+                    {
+                        id: "e15",
+                        name: "Episode 15",
+                        localName: {fr: "Episode 15 : Crise de rire"},
+                        ytids: {fr: "4gCGmqTT4pI"}
+                    },
+                    {
+                        id: "e16",
+                        name: "Episode 16",
+                        localName: {fr: "Episode 16 : Claustrophobie"},
+                        ytids: {fr: "aBREyL_1gUk"}
+                    },
+                    {
+                        id: "e17",
+                        name: "Episode 17",
+                        localName: {fr: "Episode 17 : Mémoire morte"},
+                        ytids: {fr: "C2n1l-6JrVw"}
+                    },
+                    {
+                        id: "e18",
+                        name: "Episode 18",
+                        localName: {fr: "Episode 18 : Musique mortelle"},
+                        ytids: {fr: "3fS-wMld6h0"}
+                    },
+                    {
+                        id: "e19",
+                        name: "Episode 19",
+                        localName: {fr: "Episode 19 : Frontière"},
+                        ytids: {fr: "pDUaYOF7BgE"}
+                    },
+                    {
+                        id: "e20",
+                        name: "Episode 20",
+                        localName: {fr: "Episode 20 : L'âme des robots"},
+                        ytids: {fr: "fjf4g0czaOk"}
+                    },
+                    {
+                        id: "e21",
+                        name: "Episode 21",
+                        localName: {fr: "Episode 21 : Gravité zéro"},
+                        ytids: {fr: "Q89L0MHwfnI"}
+                    },
+                    {
+                        id: "e22",
+                        name: "Episode 22",
+                        localName: {fr: "Episode 22 : Routine"},
+                        ytids: {fr: "V-XH3OPdz2Q"}
+                    },
+                    {
+                        id: "e23",
+                        name: "Episode 23",
+                        localName: {fr: "Episode 23 : 36ème dessous"},
+                        ytids: {fr: "z-28471DtyY"}
+                    },
+                    {
+                        id: "e24",
+                        name: "Episode 24",
+                        localName: {fr: "Episode 24 : Canal fantôme"},
+                        ytids: {fr: "BBYE9MI-xog"}
+                    },
+                    {
+                        id: "e25",
+                        name: "Episode 25",
+                        localName: {fr: "Episode 25 : Code Terre"},
+                        ytids: {fr: "g9kAa1Wgckg"}
+                    },
+                    {
+                        id: "e26",
+                        name: "Episode 26",
+                        localName: {fr: "Episode 26 : Faux Départ"},
+                        ytids: {fr: "PNkwbdHhawY"}
                     }
                 ]
             }
