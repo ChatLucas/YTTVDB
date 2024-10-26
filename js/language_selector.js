312f05c06af620b7c63f716b88a865b6a6a3c045
ChatLucas <108232102+ChatLucas@users.noreply.github.com>
2024-10-26 11:45:42 +0200

    Core: Store selected language as URL parameter

--- js/language_selector.js
+++ js/language_selector.js
@@ -8,8 +8,41 @@ function on_load() {
         innerHTML += "</a></li>"
     }
     document.getElementById("language-list").innerHTML = innerHTML
+
+    const parameter = new URLSearchParams(document.location.search).get('lang')
+    select_language(parameter)
 }
 
 function select_language(selected) {
+    if (db_languages[selected] == undefined) {
+        document.getElementById("language-display").innerHTML = "English"
+        window.history.replaceState('', '', updateURLParameter(window.location.href, "lang", "en"))
+        return
+    }
+
     document.getElementById("language-display").innerHTML = db_languages[selected].name
+    window.history.replaceState('', '', updateURLParameter(window.location.href, "lang", db_languages[selected].short))
+}
+
+/**
+ * http://stackoverflow.com/a/10997390/11236
+ */
+function updateURLParameter(url, param, paramVal){
+    var newAdditionalURL = "";
+    var tempArray = url.split("?");
+    var baseURL = tempArray[0];
+    var additionalURL = tempArray[1];
+    var temp = "";
+    if (additionalURL) {
+        tempArray = additionalURL.split("&");
+        for (var i=0; i<tempArray.length; i++){
+            if(tempArray[i].split('=')[0] != param){
+                newAdditionalURL += temp + tempArray[i];
+                temp = "&";
+            }
+        }
+    }
+
+    var rows_txt = temp + "" + param + "=" + paramVal;
+    return baseURL + "?" + newAdditionalURL + rows_txt;
 }
