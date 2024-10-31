c0f935593470f3e93599dff4496175eeb8345e79
ChatLucas <108232102+ChatLucas@users.noreply.github.com>
2024-10-31 07:44:44 +0100

    Catalogue: Add missing pages

--- db/catalogue.js
+++ db/catalogue.js
@@ -1,5 +1,5 @@
 let db_catalogue = [
     {name: "Code Lyoko",            available: ['en', 'fr'],    id: "code_lyoko"},
     {name: "Code Lyoko: Evolution", available: ['fr'],          id: "code_lyoko_evolution"},
-    {name: "Totally Spies",         available: ['en', 'fr'],    id: "tottaly_spies"}
+    {name: "Totally Spies",         available: ['en', 'fr'],    id: "totally_spies"}
 ]
